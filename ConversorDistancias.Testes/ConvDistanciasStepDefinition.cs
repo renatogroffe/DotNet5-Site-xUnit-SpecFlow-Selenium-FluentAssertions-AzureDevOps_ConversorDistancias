4fd97e5fdc7585f4b5d8c4dc3e0b2ddbe338b102
Renato Groffe <renato.groffe@yahoo.com.br>
2021-09-02 16:44:37 -0300

    .NET 5 + ASP.NET Core + Site MVC + xUnit + SpecFlow + Fluent Assertions+ Selenium WebDriver + Chrome Driver + Application Insights + Dockerfile + Conversor de Distâncias (milhas para Km)
    .NET 5 + ASP.NET Core + Site MVC + xUnit + SpecFlow + Fluent Assertions+ Selenium WebDriver + Chrome Driver + Application Insights + Dockerfile + Conversor de Distâncias (milhas para Km)

---
+++ ConversorDistancias.Testes/ConvDistanciasStepDefinition.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+using ConversorDistancias.Testes.PageObjects;
+
+namespace ConversorDistancias.Testes
+{
+    [Binding]
+    public sealed class ConvDistanciasStepDefinition
+    {
+        private readonly IConfiguration _configuration;
+        private double _distanciaMilhas;
+        private double _resultadoKm;
+
+        public ConvDistanciasStepDefinition()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json")
+                .AddEnvironmentVariables().Build();
+
+            CultureInfo.DefaultThreadCurrentCulture = new ("pt-BR");
+            CultureInfo.DefaultThreadCurrentUICulture = new ("pt-BR");
+        }
+
+        [Given("que o valor da distância é de (.*) milha\\(s\\)")]
+        public void PreencherDistanciaMilhas(double distanciaMilhas)
+        {
+            _distanciaMilhas = distanciaMilhas;
+        }
+
+        [When("eu solicitar a conversão desta distância")]
+        public void ProcessarConversao()
+        {
+            var conversor = new TelaConversaoDistancias(_configuration);
+
+            conversor.CarregarPagina();
+            conversor.PreencherDistanciaMilhas(_distanciaMilhas);
+            conversor.ProcessarConversao();
+            _resultadoKm = conversor.ObterDistanciaKm();
+            conversor.Fechar();
+        }
+
+        [Then("o resultado será (.*) Km")]
+        public void ValidarResultadoKm(double distanciaKm)
+        {
+            _resultadoKm.Should().Be(distanciaKm, " *** provável problema de arredondamento *** ");
+        }
+    }
+}
